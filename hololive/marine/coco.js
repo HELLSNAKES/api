7f0ac7cd53507dd59befb55215f9b4c2a9991b60
Amnesia <77104443+CuSO4-c3c@users.noreply.github.com>
2021-11-07 06:46:01 +0700

    fix

--- hololive/marine/coco.js
+++ hololive/marine/coco.js
@@ -1,5 +1,5 @@
 function randomtext() {
-    var text = `{"response": "https://emilia.cf/api/hololive/assets/gura/${Math.floor(Math.random()*342)}.jpg" }`;
+    var text = `{"response": "https://emilia.cf/api/hololive/assets/marine/${Math.floor(Math.random()*342)}.jpg" }`;
     return text;
 }
 document.getElementById('a').innerHTML = randomtext()
