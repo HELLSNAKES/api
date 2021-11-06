b78c7bf43cad9bdf213330c0427a3f21eecce8dd
Amnesia <77104443+CuSO4-c3c@users.noreply.github.com>
2021-11-07 06:45:31 +0700

    Update coco.js

--- hololive/rushia/coco.js
+++ hololive/rushia/coco.js
@@ -1,5 +1,5 @@
 function randomtext() {
-    var text = `{"response": "https://emilia.cf/api/hololive/assets/gura/${Math.floor(Math.random()*866)}.jpg" }`;
+    var text = `{"response": "https://emilia.cf/api/hololive/assets/rushia/${Math.floor(Math.random()*866)}.jpg" }`;
     return text;
 }
 document.getElementById('a').innerHTML = randomtext()
