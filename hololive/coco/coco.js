b28db1b26fe44a2ef36e8860d96a4b41b487f468
HELLSNAKE <yack752004@gmail.com>
2024-12-28 09:58:10 +0700

    fix

--- hololive/coco/coco.js
+++ hololive/coco/coco.js
@@ -1,5 +1,5 @@
 function randomtext() {
-    var text = `https://emilia.cf/api/hololive/assets/coco/${Math.floor(Math.random()*173)}.jpg`;
+    var text = `https://oniontails.github.io/api/hololive/assets/coco/${Math.floor(Math.random()*173)}.jpg`;
     return text;
 }
 document.getElementById('a').innerHTML = randomtext()
